1c79323fbadc0b6d9fd218e8a89bb4b2175ffdf7
IagoSantos <iago.olra.santos@gmail.com>
2023-05-01 16:23:52 -0300

    estilizando a lista e adicionando chamada ao github

--- src/components/RespositoryList.jsx
+++ src/components/RespositoryList.jsx
@@ -1,4 +1,9 @@
+import { useState, useEffect } from "react";
 import { RepositoryItem } from "./RespositoryItem";
+
+import "../styles/repositories.scss";
+
+//https://api.github.com/users/diego3g/repos
 
 const repository = {
   name: "unform2",
@@ -7,8 +12,18 @@ const repository = {
 };
 
 export function RepositoryList() {
+  const user = "diego3g";
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${user}/repos`)
+      .then((response) => response.json())
+      .then((data) => setRepositories(data))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
-    <section className="respository-list">
+    <section className="repository-list">
       <h1>Lista de repositórios</h1>
 
       <ul>
